10c82c262ca6c4e26c1baafd331b237f5be3a01c
Doguhan Uluca <duluca@gmail.com>
2020-04-15 03:49:19 -0400

    moar clean up, better open

--- index.js
+++ index.js
@@ -26,30 +26,35 @@ const scripts = [
   ["docker:build", "cross-conf-env docker image build . -t $npm_package_config_imageRepo:$npm_package_version"],
   ["postdocker:build", "npm run docker:tag"],
   ["docker:tag", " cross-conf-env docker image tag $npm_package_config_imageRepo:$npm_package_version $npm_package_config_imageRepo:latest"],
+  ["docker:stop", "cross-conf-env docker stop $npm_package_config_imageName || true"],
   ["docker:run", "run-s -c docker:stop docker:runHelper"],
   ["docker:runHelper", "cross-conf-env docker run -e NODE_ENV=local --rm --name $npm_package_config_imageName -d -p $npm_package_config_imagePort:$npm_package_config_internalContainerPort $npm_package_config_imageRepo"],
-  ["predocker:publish", "echo Attention! Ensure `docker login` is correct."],
+  ["predocker:publish", "echo Attention! Ensure `docker login` information is correct."],
   ["docker:publish", "cross-conf-env docker image push $npm_package_config_imageRepo:$npm_package_version"],
   ["postdocker:publish", "cross-conf-env docker image push $npm_package_config_imageRepo:latest"],
-  ["docker:stop", "cross-conf-env docker stop $npm_package_config_imageName || true"],
-  ["docker:clean", "cross-conf-env docker rm -f $npm_package_config_imageName || true"],
-  ["predocker:taillogs", "echo Web Server Logs:"],
-  ["docker:taillogs", "cross-conf-env docker logs -f $npm_package_config_imageName"],
-  ["docker:open:win", "echo Trying to launch on Windows && timeout 2 && start http://localhost:%npm_package_config_imagePort%"],
-  ["docker:open:mac", "echo Trying to launch on MacOS && sleep 2 && URL=http://localhost:$npm_package_config_imagePort && open $URL"],
+  ["docker:taillogs", "echo Web Server Logs: && cross-conf-env docker logs -f $npm_package_config_imageName"],
+  ["docker:open", "sleep 2 && cross-conf-env open-cli http://localhost:$npm_package_config_imagePort"],
   ["docker:debugmessage", "echo Docker Debug Completed Successfully! Hit Ctrl+C to terminate log tailing."],
-  ["predocker:debug", "run-s docker:build docker:run"],
-  ["docker:debug", "run-s -cs docker:open:win docker:open:mac docker:debugmessage docker:taillogs"]
+  ["docker:debug", "run-s -cs docker:build docker:run docker:open docker:debugmessage docker:taillogs"]
 ]
 
 
 function configureNpmScripts() {
   const pkg = packageJson();
 
+  // clean up old scripts
+  pkg.removeScript('docker:open:win')
+  pkg.removeScript('docker:open:mac')
+  pkg.removeScript('predocker:taillogs')
+  pkg.removeScript('predocker:debug')
+  pkg.removeScript('predocker:clean')
   
   scripts.forEach(e => pkg.setScript(e[0], e[1]).save())
 
-
+  if(!pkg.get('config')) {
+    pkg.set('config', {})
+  }
+  
   config.forEach(e => {
     if(!pkg.get(`config.${e[0]}`)) {
       pkg.set(`config.${e[0]}`, e[1])
@@ -58,7 +63,7 @@ function configureNpmScripts() {
 }
 
 function configureCommonNpmPackages() {
-  const commonNpm = ["cross-conf-env", "npm-run-all", "dev-norms"];
+  const commonNpm = ["cross-conf-env", "npm-run-all", "dev-norms", "open-cli"];
   install(commonNpm);
 }
 
