2d7a89b5b2e6b19b707bc594a2711a017a5631ca
Doguhan Uluca <duluca@gmail.com>
2021-08-01 13:48:44 -0400

    add test

--- tests/test-local.sh
+++ tests/test-local.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+mkdir -p node_modules/mrm-task-58f5f082-ee83-4be4-9212-b3be82d3b592
+
+cp index.js node_modules/mrm-task-58f5f082-ee83-4be4-9212-b3be82d3b592/index.js
+
 mkdir -p tmp
 
 cd tmp
 
 npm init -y
 
-node ../index.js
+npx mrm mrm-task-58f5f082-ee83-4be4-9212-b3be82d3b592
 
 cat package.json
